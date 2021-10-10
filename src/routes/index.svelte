7a461f9fadef36f4dff03a17b962d405781f7d95
brittneypostma <brittney.postma@hotmail.com>
2021-10-10 18:03:14 -0400

    progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,27 @@
-<script>
-	import { fade } from 'svelte/transition';
-</script>
-
-<h1 transition:fade>Welcome</h1>
+<article><h1>Welcome</h1></article>
 
 <style>
 	h1 {
 		font-size: clamp(50px, calc(3.125rem + ((1vw - 2.8px) * 6.7204)), 100px);
 		min-height: 0vw;
-		height: 100vh;
-		text-align: center;
+		height: 100%;
 		width: 100%;
+		text-align: center;
 		margin-inline: auto;
-		/* position: absolute;
-		left: 0;
-		bottom: 2.5%; */
+		animation: slideUp 1s cubic-bezier(0, 1, 0.5, 1);
+	}
+	article {
+		height: 100vh;
+		overflow-y: hidden;
+	}
+	@keyframes slideUp {
+		0% {
+			opacity: 0;
+			transform: translateY(100%);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(0);
+		}
 	}
 </style>
