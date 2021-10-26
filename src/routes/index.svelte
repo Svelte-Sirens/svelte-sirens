57b73501b4b5385ab5aad3ecc3ae08a4b86c5704
brittneypostma <brittney.postma@hotmail.com>
2021-10-26 07:28:56 -0400

    new discord link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 	<section>
 		<h1>Svelte Sirens</h1>
 		<h2>A Svelte Society for women & non-binary</h2>
-		<a rel="noopener noreffer" target="_blank" href="https://discord.gg/NwKTzASz"
+		<a rel="noopener noreffer" target="_blank" href="https://discord.gg/DDjEdrwW"
 			>Join the community</a
 		>
 	</section>
