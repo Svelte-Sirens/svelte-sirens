949d6e1657a18f717ce86b7e8fd94376bddd8155
GHOSt <ghostdevbusiness@gmail.com>
2021-11-19 19:42:45 +0000

    fix: blur

--- src/lib/components/bubbles/Bubble.svelte
+++ src/lib/components/bubbles/Bubble.svelte
@@ -62,7 +62,7 @@
 			border-radius: 100%;
 			box-shadow: 0 10px 15px rgba(0, 0, 0, 0.2), inset 0px 5px 15px 2px rgba(255, 255, 255, 1);
 
-			backdrop-filter: blur(3px);
+			backdrop-filter: blur(1px);
 			filter: blur(2px);
 		}
 	}
