229542bf37829c75ea4cf8d82e605b9a99e9e10c
brittneypostma <brittney.postma@hotmail.com>
2021-11-30 15:13:46 -0500

    added title

--- src/routes/talks/index.svelte
+++ src/routes/talks/index.svelte
@@ -16,18 +16,19 @@
 
 <h1>Talks</h1>
 <article>
-	<section>
-		{#each talks as { talkUrl }}
+	<section class="glass">
+		{#each talks as { talkUrl, title }}
 			{#if talkUrl}
-				<iframe
-					width="100%"
-					height="350"
-					src={talkUrl}
-					title="YouTube video player"
-					frameborder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
-				/>
+				<h2>{title}</h2>
+				<div class="video-wrapper">
+					<iframe
+						src={talkUrl}
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen
+					/>
+				</div>
 			{/if}
 		{/each}
 	</section>
@@ -52,5 +53,29 @@
 		display: grid;
 		gap: var(--gap-4);
 		grid-template-columns: minmax(0, 1fr);
+	}
+
+	.glass {
+		display: grid;
+		gap: var(--gap-4);
+		grid-template-columns: minmax(0, 1fr);
+
+		h2 {
+			font-family: var(--font-body);
+		}
+
+		.video-wrapper {
+			position: relative;
+			padding-bottom: 56.25%;
+			height: 0;
+		}
+
+		iframe {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
 	}
 </style>
