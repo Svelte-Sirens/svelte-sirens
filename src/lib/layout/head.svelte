9abee519850568c1c4f8e0820d81c033c50cbde0
brittneypostma <brittney.postma@hotmail.com>
2021-12-06 12:35:53 -0500

    fixed title again and url

--- src/lib/layout/head.svelte
+++ src/lib/layout/head.svelte
@@ -8,27 +8,30 @@
 
 	const img = 'https://www.sveltesirens.dev/images/android-chrome-192x192.png';
 	const desc = 'Svelte Sirens is a Svelte Society for women, non-binary people, & allies.';
-	let title =
+	$: title =
 		$page.path === '/'
 			? 'Svelte Sirens'
-			: `${$page.path.substring(1).replace('s/', ' | ').replace(/-/g, ' ')} | Svelte Sirens`;
+			: `${capitalize(
+					$page.path.substring(1).replace('s/', ' | ').replace(/-/g, ' ')
+			  )} | Svelte Sirens`;
+	console.log(title);
 </script>
 
 <svelte:head>
 	<meta name="description" content={desc} />
-	<meta name="title" content={capitalize(title)} />
+	<meta name="title" content={title} />
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://www.sveltesirens.dev/${page.path}" />
-	<meta property="og:title" content={capitalize(title)} />
+	<meta property="og:url" content={`https://www.sveltesirens.dev${$page.path}`} />
+	<meta property="og:title" content={title} />
 	<meta property="og:description" content={desc} />
 	<meta property="og:image" content={img} />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary" />
 	<meta property="twitter:url" content="https://twitter.com/SvelteSirens" />
-	<meta property="twitter:title" content={capitalize(title)} />
+	<meta property="twitter:title" content={title} />
 	<meta property="twitter:description" content={desc} />
 	<meta property="twitter:image" content={img} />
 	<meta name="twitter:image:alt" content="Svelte Sirens" />
@@ -40,5 +43,5 @@
 		href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700&family=Red+Hat+Text:wght@300;400;700&display=swap"
 		rel="stylesheet"
 	/>
-	<title>{capitalize(title)}</title>
+	<title>{title}</title>
 </svelte:head>
