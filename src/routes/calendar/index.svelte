dbabb098e57b27c247e08cab9e29440b2ba50a5a
brittneypostma <brittney.postma@hotmail.com>
2022-01-18 13:54:48 -0500

    testing build

---
+++ src/routes/calendar/index.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+	export async function load() {
+		return {
+			status: 301,
+			redirect: 'https://calendar.google.com/calendar/u/3?cid=c3ZlbHRlc2lyZW5zQGdtYWlsLmNvbQ'
+		};
+	}
+</script>
