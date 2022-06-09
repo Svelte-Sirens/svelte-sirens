490965af0356ba47c3cd9a74904bc106e8b8d822
GHOSt <ghostdevbusiness@gmail.com>
2022-06-09 13:23:54 +0100

    fix: update configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,25 @@
-/** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
-import path from 'path';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
 		}),
-		vite: {
-			resolve: {
-				alias: {
-					// these are the aliases and paths to them
-					'@api': path.resolve('./src/lib/api'),
-					'@components': path.resolve('./src/lib/components'),
-					'@gql': path.resolve('./src/lib/utils/gql'),
-					'@layout': path.resolve('./src/lib/layout'),
-					'@lib': path.resolve('./src/lib'),
-					'@utils': path.resolve('./src/lib/utils'),
-					'@styles': path.resolve('./src/lib/styles'),
-					'@stores': path.resolve('./src/lib/stores')
-				}
-			}
+
+		alias: {
+			'@api': path.resolve('./src/lib/api'),
+			'@components': path.resolve('./src/lib/components'),
+			'@gql': path.resolve('./src/lib/utils/gql'),
+			'@layout': path.resolve('./src/lib/layout'),
+			'@lib': path.resolve('./src/lib'),
+			'@utils': path.resolve('./src/lib/utils'),
+			'@styles': path.resolve('./src/lib/styles'),
+			'@stores': path.resolve('./src/lib/stores')			
 		}
 	},
 
