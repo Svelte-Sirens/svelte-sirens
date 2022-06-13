2ebf2c391daa279b1becd2fef7b457b276f44011
GHOSt <ghostdevbusiness@gmail.com>
2022-06-13 19:57:21 +0100

    test

--- svelte.config.js
+++ svelte.config.js
@@ -5,11 +5,7 @@ import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 
 		alias: {
 			'@api': path.resolve('./src/lib/api'),
