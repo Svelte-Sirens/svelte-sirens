cecc02a8936e3e4dc1e1a26cd201f260cc9c64db
spences10 <spences10apps@gmail.com>
2022-07-10 18:34:48 +0100

    feat: :recycle: add data files
    use in place of GraphCMS

---
+++ src/lib/data/events.js
@@ -0,0 +1,142 @@
+export const events = [
+	{
+		slug: 'the-story-behind-the-unofficial-svelte-newsletter',
+		title: 'The story behind the (unofficial) Svelte newsletter',
+		id: 'cl3a5wx41006d0ciocaem2nud',
+		datetime_event: '2022-05-27T18:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/watch?v=aK0xXm3hPxk',
+		speakers: [
+			{
+				name: 'Dani Sandoval',
+				handle: '@d3sandoval',
+				handleUrl: 'https://twitter.com/d3sandoval',
+				picture: {
+					id: 'cl3a5yp4300sh0ciodeg0fw2d',
+					url: 'https://media.graphassets.com/suZRKKnR1epl9c80qM4p',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/suZRKKnR1epl9c80qM4p'
+				}
+			}
+		]
+	},
+	{
+		slug: 'sveltekit-sanityio-a-match-made-in-heaven',
+		title: 'SvelteKit + Sanity.io; a match made in heaven',
+		id: 'cl2s4h9fms8qv0ckc44o1czue',
+		datetime_event: '2022-05-13T17:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/watch?v=DGi7FtBr2DE',
+		speakers: [
+			{
+				name: 'Jacob Størdahl',
+				handle: '@stordahl_',
+				handleUrl: 'https://twitter.com/stordahl_',
+				picture: {
+					id: 'cl0qsj8gy11o10cisk2yq32i9',
+					url: 'https://media.graphassets.com/fY2Fdx63SzS81MYXAN0F',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/fY2Fdx63SzS81MYXAN0F'
+				}
+			}
+		]
+	},
+	{
+		slug: 'testing-in-svelte',
+		title: 'Building Advanced Landing Pages',
+		id: 'cl1f3przhqx5n0bir33dyd2ok',
+		datetime_event: '2022-04-15T17:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/watch?v=D8oo5dwXMQ4',
+		speakers: [
+			{
+				name: 'Amy Dutton',
+				handle: 'selfteachme',
+				handleUrl: 'https://twitter.com/selfteachme',
+				picture: {
+					id: 'ckyw0wwfk6wp40d267bqvb1t5',
+					url: 'https://media.graphassets.com/6FjXDBLSXyzD7lDSlbjj',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/6FjXDBLSXyzD7lDSlbjj'
+				}
+			}
+		]
+	},
+	{
+		slug: 'build-in-3d-with-svelte',
+		title: 'Build in 3D with Svelte',
+		id: 'ckzzzf5zk37kv0b816apivf9z',
+		datetime_event: '2022-03-03T20:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/watch?v=7PJ_28fgLp8',
+		speakers: [
+			{
+				name: 'Scott Tolinski',
+				handle: '@stolinski',
+				handleUrl: 'https://twitter.com/stolinski',
+				picture: {
+					id: 'ckyw0r1s86ssi0c85hq4yfqbn',
+					url: 'https://media.graphassets.com/6oZ9ISlTXS3yAZvqmSCu',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/6oZ9ISlTXS3yAZvqmSCu'
+				}
+			}
+		]
+	},
+	{
+		slug: 'all-about-adapters-deploying-in-sveltekit',
+		title: 'All about Adapters - Deploying in SvelteKit',
+		id: 'ckw2n9usgy9bu0g89wrhpn3se',
+		datetime_event: '2022-01-18T18:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/embed/SUrFDhhsJN',
+		speakers: [
+			{
+				name: 'Steph Dietz',
+				handle: '@steph_dietz_',
+				handleUrl: 'https://twitter.com/steph_dietz_',
+				picture: {
+					id: 'ckw2ajstkrlyb0g898zuhesth',
+					url: 'https://media.graphassets.com/tPAnKWpmRMGSxAm1O2e1',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/tPAnKWpmRMGSxAm1O2e1'
+				}
+			}
+		]
+	},
+	{
+		slug: 'routes-for-svelte-with-routify',
+		title: 'Exploring Routify in Svelte Kit',
+		id: 'ckw2nhvdcye280g890hkpf4c7',
+		datetime_event: '2021-12-17T18:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/embed/epVRgjhjI7M',
+		speakers: [
+			{
+				name: 'Willow (GHOST)',
+				handle: '@onlyspaceghost',
+				handleUrl: 'https://twitter.com/onlyspaceghost',
+				picture: {
+					id: 'ckw2ajxg8rlz90g89ing9oioy',
+					url: 'https://media.graphassets.com/aOOamQI1SFqX6bKIY6YC',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/aOOamQI1SFqX6bKIY6YC'
+				}
+			}
+		]
+	},
+	{
+		slug: 'building-design-systems-in-sveltekit',
+		title: 'Building Design Systems in SvelteKit',
+		id: 'ckw2jjkoovc470b80cpobm450',
+		datetime_event: '2021-11-29T18:00:00+00:00',
+		eventUrl: 'https://www.youtube.com/embed/3bjVQJwF6O8',
+		speakers: [
+			{
+				name: 'Brittney Postma',
+				handle: '@brittneypostma',
+				handleUrl: 'https://twitter.com/BrittneyPostma',
+				picture: {
+					id: 'ckw2a0c7crcjv0b72qihasavr',
+					url: 'https://media.graphassets.com/pcYZgGxRTYqY9YVKhG5O',
+					small:
+						'https://media.graphassets.com/resize=fit:clip,height:200,width:200/pcYZgGxRTYqY9YVKhG5O'
+				}
+			}
+		]
+	}
+];
---
+++ src/lib/data/index.js
@@ -0,0 +1,3 @@
+export { default as Events } from './events';
+export { default as Speakers } from './speakers';
+export { default as Streams } from './streams';
