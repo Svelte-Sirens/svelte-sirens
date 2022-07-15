304a243c64edd02673a772eac990d34dbb047508
brittneypostma <bdesignwebdev@gmail.com>
2022-07-15 14:59:46 -0400

    trying adapter netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,34 +1,34 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-import path from 'path';
+import adapter from '@sveltejs/adapter-netlify'
+import preprocess from 'svelte-preprocess'
+import path from 'path'
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		alias: {
-			'@components': path.resolve('./src/lib/components'),
-			'@layout': path.resolve('./src/lib/layout'),
-			'@lib': path.resolve('./src/lib'),
-			'@utils': path.resolve('./src/lib/utils'),
-			'@styles': path.resolve('./src/lib/styles'),
-			'@stores': path.resolve('./src/lib/stores'),
-			'@data': './src/data',
-		},
+    alias: {
+      '@components': path.resolve('./src/lib/components'),
+      '@layout': path.resolve('./src/lib/layout'),
+      '@lib': path.resolve('./src/lib'),
+      '@utils': path.resolve('./src/lib/utils'),
+      '@styles': path.resolve('./src/lib/styles'),
+      '@stores': path.resolve('./src/lib/stores'),
+      '@data': './src/data',
+    },
 
-		prerender: {
-			default: true,
-			crawl: true
-		}
-	},
+    prerender: {
+      default: true,
+      crawl: true
+    }
+  },
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
-};
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ]
+}
 
-export default config;
+export default config
