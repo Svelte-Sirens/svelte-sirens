9f7c9129ef51d176d944216b96e8ef5d5d599f42
Luke Shafer <lukealanshafer@gmail.com>
2022-10-02 14:36:34 -0400

    refactor: add type to calendar server code

--- src/routes/calendar/+page.server.ts
+++ src/routes/calendar/+page.server.ts
@@ -1,5 +1,9 @@
 import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export async function load() {
-	throw redirect(301, 'https://calendar.google.com/calendar/u/3?cid=c3ZlbHRlc2lyZW5zQGdtYWlsLmNvbQ');
-}
+export const load: PageServerLoad = async () => {
+	throw redirect(
+		301,
+		'https://calendar.google.com/calendar/u/3?cid=c3ZlbHRlc2lyZW5zQGdtYWlsLmNvbQ'
+	);
+};
