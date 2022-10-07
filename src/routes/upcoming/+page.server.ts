4db3799f3d3a03eb40b084a2bb6cbc9fcb5f4c70
Brittney Postma <bdesignedwebdev@gmail.com>
2022-10-07 13:56:36 +0000

    removed comment

--- src/routes/upcoming/+page.server.ts
+++ src/routes/upcoming/+page.server.ts
@@ -1,6 +1,5 @@
 import type { PageServerLoad } from './$types';
 import { getUpcoming } from '@data/utils/time';
-// import { streams } from '@data/streams';
 import { events } from '@data/events';
 
 export const load: PageServerLoad = () => {
