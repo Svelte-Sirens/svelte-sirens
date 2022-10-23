18025dad9a75fbf1791f6363cee730633abddb0b
brittneypostma <bdesignwebdev@gmail.com>
2022-10-23 18:43:16 -0400

    removed ssr false

--- src/routes/bubble-pop/+page.server.ts
+++ src/routes/bubble-pop/+page.server.ts
@@ -1,8 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const ssr = false;
-
 export const load: PageServerLoad = async () => {
 	throw redirect(301, 'https://bubble-pop-svelte.netlify.app/');
 };
