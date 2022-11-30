3d13a4ce0eb9a363df755b595f9cfe460b68e0fd
GHOST <ghostdevbusiness@gmail.com>
2022-11-30 21:43:51 +0000

    feat: update ui

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,43 +6,87 @@
 </script>
 
 <section>
-	<h1>Get in Touch</h1>
+	<h1 style="margin-bottom: 16px; margin-top: 0px;">
+		Submit A Talk
+	</h1>
+
 	<form method="POST" use:enhance>
 		<label>
 			Name *
-			<input required value={form?.name || ''} name="name" type="text" placeholder="John Doe" />
+
+			<input
+				class:error={form?.errors?.name}
+				value={form?.data?.name || ''}
+				required
+				name="name"
+				type="text"
+				placeholder="John Doe"
+			/>
+
+			{#if form?.errors?.name}
+				<p class="error-text">{form?.errors?.name[0]}</p>
+			{/if}
 		</label>
 
 		<label>
 			Email *
 
 			<input
+				class:error={form?.errors?.email}
+				value={form?.data?.email || ''}
 				required
-				value={form?.email || ''}
 				name="email"
 				type="email"
 				placeholder="hello@example.com"
 			/>
+
+			{#if form?.errors?.email}
+				<p class="error-text">{form?.errors?.email[0]}</p>
+			{/if}
 		</label>
 
 		<label>
 			Discord
-			<input value={form?.discord || ''} name="discord" type="text" placeholder="GHOST#7524" />
+
+			<input
+				class:error={form?.errors?.discord}
+				value={form?.data?.discord || ''}
+				name="discord"
+				type="text"
+				required
+				placeholder="GHOST#7524"
+			/>
+
+			{#if form?.errors?.discord}
+				<p class="error-text">{form?.errors?.discord[0]}</p>
+			{/if}
 		</label>
 
 		<label>
 			Message *
-			<textarea required value={form?.idea || ''} name="idea" placeholder="Tell us your ideas!" />
+
+			<textarea
+				class:error={form?.errors?.idea}
+				value={form?.data?.idea || ''}
+				name="idea"
+				required
+				placeholder="Tell us your ideas!"
+			/>
+
+			{#if form?.errors?.idea}
+				<p class="error-text">{form?.errors?.idea[0]}</p>
+			{/if}
 		</label>
 
 		<button class="btn-primary">Submit!</button>
+
+		{#if form?.success}
+			<p class="success-text">
+				Success! Your talk has been submitted
+			</p>
+		{/if}
 	</form>
 </section>
-{#if form?.success}
-	<p>You successfully submitted a talk</p>
-{:else if form?.error}
-	<p>There was an error: {form?.error}</p>
-{/if}
 
 <style>
 	section {
@@ -54,6 +98,17 @@
 		max-width: 80ch;
 		border: var(--border-size-1) solid var(--light-teal);
 		border-radius: var(--radius-conditional-3);
+	}
+
+	.error-text {
+		color: var(--red);
+		text-align: left;
+		font-size: var(--font-size-1);
+		font-weight: 500;
+	}
+
+	.success-text {
+		margin-top: 16px;
 	}
 
 	@media (min-width: 1024px) {
@@ -86,6 +141,11 @@
 		border: var(--size-1) solid transparent;
 	}
 
+	input.error,
+	textarea.error {
+		border-color: var(--red);
+	}
+
 	textarea {
 		min-height: 150px;
 	}
@@ -105,6 +165,6 @@
 
 	button {
 		justify-self: center;
-		width: auto;
+		margin-top: 16px;
 	}
 </style>
