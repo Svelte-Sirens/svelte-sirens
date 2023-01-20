39da8f0b4f59afb7a1920aa8811708293bf76b89
brittneypostma <bdesignedwebdev@gmail.com>
2023-01-20 11:37:26 -0500

    removed comments and formatted layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import { onMount, onDestroy } from 'svelte';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.css';
-	import { AppShell } from '@skeletonlabs/skeleton';
 	import Head from '$components/Head.svelte';
 	import Header from './Header.svelte';
 	import Bubbles from '$components/bubbles/Bubbles.svelte';
@@ -24,14 +23,10 @@
 </script>
 
 <Head />
-<!-- <AppShell>
-	<svelte:fragment slot="header"> -->
 <Header {yScroll} />
-<!-- </svelte:fragment> -->
 <main id="page" class="h-full overflow-y-auto w-full pb-48">
 	<div class="grid gap-10 lg:gap-20 p-4 xl:p-0 mb-6 max-w-7xl mx-auto">
 		<slot />
 	</div>
 </main>
 <Bubbles />
-<!-- </AppShell> -->
